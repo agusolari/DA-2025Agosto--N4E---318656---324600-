39187a2021526122a2c9414cd043574371730b15
Agustín Solari <AS318656@fi365.ort.edu.uy>
2025-10-18 21:30:22 -0300

    commit inicial

---
+++ src/main/java/obligatorioAraujoSolari/Obligatorio/ObligatorioApplication.java
@@ -0,0 +1,13 @@
+package obligatorioAraujoSolari.Obligatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligatorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/obligatorioAraujoSolari/Obligatorio/ObligatorioApplicationTests.java
@@ -0,0 +1,13 @@
+package obligatorioAraujoSolari.Obligatorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligatorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

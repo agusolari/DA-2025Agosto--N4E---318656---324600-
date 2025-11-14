bb902336b9db4837f44121382e42ee0e7ba126a7
Hernan Araujo <hernan.araujo@cencosud.com.uy>
2025-11-13 22:59:33 -0300

    Agregar clase TransitoDto para representar datos de tránsito

---
+++ src/main/java/obligatorioAraujoSolari/dtos/TransitoDto.java
@@ -0,0 +1,38 @@
+package obligatorioAraujoSolari.dtos;
+
+import java.time.LocalDateTime;
+
+import obligatorioAraujoSolari.Obligatorio.dominio.Transito;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TransitoDto {
+    private String vehiculoMatricula;
+    private String puestoPeajeNombre;
+    private Double tarifa;
+    private Double descuento;
+    private Double montoPagado;
+    private LocalDateTime fechaTransito;
+    private String bonificacion;
+
+    public TransitoDto() {}
+
+    public TransitoDto(Transito transito) {
+        this.vehiculoMatricula = transito.getVehiculo() != null ? transito.getVehiculo().getMatricula() : null;
+        this.puestoPeajeNombre = transito.getPuestoPeaje() != null ? transito.getPuestoPeaje().getNombre() : null;
+        this.tarifa = transito.getTarifa() != null ? transito.getTarifa().getMonto() : 0.0;
+        this.fechaTransito = transito.getFechaHora();
+        
+        // Usar el monto pagado guardado en el tránsito
+        this.montoPagado = transito.calcularTarifaFinal();
+        this.descuento = this.tarifa - this.montoPagado;
+        
+        if (transito.getBonificacion() != null) {
+            this.bonificacion = transito.getBonificacion().getNombre();
+        } else {
+            this.bonificacion = "Ninguna";
+        }
+    }
+}
